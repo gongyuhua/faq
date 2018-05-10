6f0ce2d3058652c8a3b3dd0b2c78df4a1c8bb7df
gongyuhua <yg348@njit.edu>
2018-05-10 15:33:00 -0400

    create sidebar add recent added users and do view composer.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,16 +58,6 @@
         <br>
         <br>
 
-        <div class="sidebar-module">
-            <h4>Recent Added Users</h4>
-            <ol class="list-unstyled">
-                @foreach($archives as $archives)
-                    <li>
-                        <a href="#">{{$archives['email1']}}</a>
 
-                    </li>
-                @endforeach
-            </ol>
-        </div>
 
 @endsection
